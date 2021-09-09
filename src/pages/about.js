2cdc9765611472780bc2504cbe096ade4f36eae5
Nirmal Savinda <nirmalsavinda29@gmail.com>
2021-09-09 16:05:54 +0800

    site

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1> About</h1>
+                <p>Build, Deploy, and Host your site on Gatsby Cloud for a fast, secure, and scalable Gatsby experience.</p>
+                <p>Build, Deploy, and Host your site on Gatsby Cloud for a fast, secure, and scalable Gatsby experience.</p>
+                <p>Build, Deploy, and Host your site on Gatsby Cloud for a fast, secure, and scalable Gatsby experience.</p>
+
+            </div>
+        </Layout>
+    )
+}
